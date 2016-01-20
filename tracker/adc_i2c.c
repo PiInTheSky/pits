f559743d3b01c4b0b7d05e911e843005a370d602
phl0 <florian@florian-wolters.de>
2016-01-20 16:39:18 +0100

    Correct current measurement

--- tracker/adc_i2c.c
+++ tracker/adc_i2c.c
@@ -41,7 +41,7 @@ unsigned int I2CAnalogRead (int fd, int chan)
 	unsigned int value;
 	
 	wiringPiI2CWrite(fd, 0x80 +			// bit  7   -  1 = start single-shot conversion
-						 (chan << 6) +	// bits 6,5 - 00 = channel 1, 01 = channel 2
+						 (chan << 5) +	// bits 6,5 - 00 = channel 1, 01 = channel 2
 						 0x00 +			// bit 4    -  0 = single shot mode
 						 0x04 +			// bits 3,2 - 10 = 16-bit data (14 measurements per second)
 						 0x00);			// bits 1,0 - 00 = Gain x1
@@ -92,7 +92,7 @@ void *I2CADCLoop(void *some_void_ptr)
 			GPS->BatteryVoltage = BatteryVoltage;
 			// printf("Battery Voltage = %lf\n", BatteryVoltage);
 			
-			BoardCurrent = ReadI2CADC(fd, 1, 1.4);
+			BoardCurrent = ReadI2CADC(fd, 1, 120000.0);
 			GPS->BoardCurrent = BoardCurrent;
 			// printf("Board Current = %lf\n", BoardCurrent);
 
@@ -103,4 +103,4 @@ void *I2CADCLoop(void *some_void_ptr)
 	}
 
     return 0;
-}
+}
