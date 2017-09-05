e050fcb9f3884afd1968f47dbe516d3f2b7de577
Dave Akerman <dave@sccs.co.uk>
2017-09-05 11:26:57 +0000

    Print field list on first sentence

--- tracker/misc.c
+++ tracker/misc.c
@@ -751,7 +751,8 @@ int devicetree(void)
 }
 
 int BuildSentence(unsigned char *TxLine, int Channel, struct TGPS *GPS)
-{
+{	
+static char *Channels[6] = {"RTTY", "APRS", "LORA0", "LORA1", "FULL", "PIPE"};
 	static char ExternalFields[100];
 	static FILE *ExternalFile=NULL;
 	static int FirstTime=1;
@@ -777,6 +778,9 @@ int BuildSentence(unsigned char *TxLine, int Channel, struct TGPS *GPS)
 	
 	ExtraFields7 = "";
 	
+	if (Config.Channels[Channel].SentenceCounter == 1) printf("%s: ID,Ctr,Time,Lat,Lon,Alt,Sped,Head,Sats,Int.Temp", Channels[Channel]);
+	
+	
 	// Battery voltage and current, if available
 	if ((Config.BoardType == 3) || (Config.BoardType == 4) || (Config.DisableADC))
 	{
@@ -787,28 +791,33 @@ int BuildSentence(unsigned char *TxLine, int Channel, struct TGPS *GPS)
 		// Pi A or B.  Only Battery Voltage on the PITS
 		
 		sprintf(ExtraFields1, ",%.3f", GPS->BatteryVoltage);
+		if (Config.Channels[Channel].SentenceCounter == 1) printf(",Volts");
 	}
 	else
 	{
 		// Pi A+ or B+ (V1 or V2 or V3).  Full ADC for voltage and current
 
 		sprintf(ExtraFields1, ",%.1f,%.3f", GPS->BatteryVoltage, GPS->BoardCurrent);
+		if (Config.Channels[Channel].SentenceCounter == 1) printf(",Volts,Current");
 	}
 	
 	// BMP Pressure/Temperature/Humidity, if available
 	if (Config.EnableBME280)
 	{
 		sprintf(ExtraFields2, ",%.1f,%.0f,%0.1f", GPS->BMP180Temperature, GPS->Pressure, GPS->Humidity);
+		if (Config.Channels[Channel].SentenceCounter == 1) printf(",BME.Temp,Pressure,Humidity");
 	}
 	else if (Config.EnableBMP085)
 	{
 		sprintf(ExtraFields2, ",%.1f,%.0f", GPS->BMP180Temperature, GPS->Pressure);
+		if (Config.Channels[Channel].SentenceCounter == 1) printf(",BMP.Temp,Pressure");
 	}
 	
 	// Second DS18B20 Temperature Sensor, if available
 	if (GPS->DS18B20Count > 1)
 	{
 		sprintf(ExtraFields3, ",%3.1f", GPS->DS18B20Temperature[Config.ExternalDS18B20]);
+		if (Config.Channels[Channel].SentenceCounter == 1) printf(",Ext.Temp");
 	}
 	
 	// Landing Prediction, if enabled
@@ -820,6 +829,7 @@ int BuildSentence(unsigned char *TxLine, int Channel, struct TGPS *GPS)
 																GPS->PredictedLongitude,
 																GPS->PredictedLandingSpeed,
 																GPS->TimeTillLanding);
+		if (Config.Channels[Channel].SentenceCounter == 1) printf(",CDA,Pred.Lat,Pred.Lon,Pred.Land,Pred.TTL");
 	}
 	
 	// Specific to LoRa Uplink
@@ -832,11 +842,13 @@ int BuildSentence(unsigned char *TxLine, int Channel, struct TGPS *GPS)
 			sprintf(ExtraFields5, ",%d,%d,%d", Config.LoRaDevices[LoRaChannel].LastPacketRSSI,
 											   Config.LoRaDevices[LoRaChannel].LastPacketSNR,
 											   Config.LoRaDevices[LoRaChannel].PacketCount);
+			if (Config.Channels[Channel].SentenceCounter == 1) printf(",RSSI,SNR,Packets");
 		}
 
 		if (Config.LoRaDevices[LoRaChannel].EnableMessageStatus)
 		{	
 			sprintf(ExtraFields6, ",%s", Config.LoRaDevices[LoRaChannel].LastCommand);
+			if (Config.Channels[Channel].SentenceCounter == 1) printf(",Command");
 		}
 		
 	}
@@ -879,8 +891,11 @@ int BuildSentence(unsigned char *TxLine, int Channel, struct TGPS *GPS)
 			}
 			fseek(ExternalFile, 0, SEEK_END);
 			// clearerr(ExternalFile);
+			if (Config.Channels[Channel].SentenceCounter == 1) printf(",[ExternalCSV]");
 		}
 	}	
+
+	if (Config.Channels[Channel].SentenceCounter == 1) printf("\n");
 	
 	// Bouy mode or normal mode ?
 	if ((Config.BuoyModeAltitude > 0) && (GPS->Altitude < Config.BuoyModeAltitude))
@@ -897,7 +912,7 @@ int BuildSentence(unsigned char *TxLine, int Channel, struct TGPS *GPS)
 		#ifdef EXTRAS_PRESENT
 			ExtraFields7 = misc_get_sentence_fields(GPS);
 		#endif
-		
+			
 		sprintf((char *)TxLine, "$$%s,%d,%s,%7.5lf,%7.5lf,%5.5" PRId32  ",%d,%d,%d,%3.1f%s%s%s%s%s%s%s%s",
 				Config.Channels[Channel].PayloadID,
 				Config.Channels[Channel].SentenceCounter,
