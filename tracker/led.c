7fc1d7eba6dafe27dfaa69cec348a2450f89f60d
YO3ICT <56532823+YO3ICT@users.noreply.github.com>
2020-02-02 15:50:28 +0200

    Cutdown/Startup buzzer
    Added cutdown servo routine to trigger at specific altitude/latitude/longitude.
    Buzzer now beeps one time after Pi successfully boots.

--- tracker/led.c
+++ tracker/led.c
@@ -19,6 +19,8 @@ void *LEDLoop(void *some_void_ptr)
 {
 	struct TGPS *GPS;
 	int Flash=0;
+	int peak_alt=0;
+	int servo_open=0;
 
 	GPS = (struct TGPS *)some_void_ptr;
 
@@ -26,11 +28,33 @@ void *LEDLoop(void *some_void_ptr)
 	pinMode (Config.LED_Warn, OUTPUT);
 	pinMode (Config.LED_OK, OUTPUT);
 	
+	pinMode (26, PWM_OUTPUT); //GPIO 26 for cutdown servo
+	pwmSetMode(PWM_MODE_MS);
+	pwmSetRange(2000);
+	pwmSetClock(192);
+	pwmWrite (26, 150); //Set servo PWM for 'closed' position
+
+	digitalWrite (Config.PiezoPin, 1);
+	sleep(1);
+	digitalWrite (Config.PiezoPin, 0);
+	
 	while (1)
 	{
 		digitalWrite (Config.LED_Warn, (GPS->Satellites < 4) && (GPS->Altitude < 2000) && (GPS->MessageCount & 1));	
 		digitalWrite (Config.LED_OK, (GPS->Satellites >= 4) && (GPS->Altitude < 2000) && (Flash ^= 1));	
-
+		
+		if ((servo_open == 0) && (GPS->Satellites >= 5))
+			{
+				if (GPS->Altitude > peak_alt)
+					peak_alt = GPS->Altitude;
+				
+				if ((GPS->Altitude > 20000) || (peak_alt > (GPS->Altitude + 300)) || (GPS->Latitude > 45.1) || (GPS->Longitude > 26.8))
+				{
+					pwmWrite (26, 225); //Set servo PWM for 'open' position
+					servo_open=1;
+				}
+			}
+		
 		sleep(1);
 	}
 
